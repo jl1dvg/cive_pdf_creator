242bca1e5dccf6f639a57a3aed20a8d10915fb1c
jorgeluisdevera <jl1dvg@gmail.com>
2024-08-27 21:04:57 -0500

    Primer PDF

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php
+require 'vendor/autoload.php';
+
+use Mpdf\Mpdf;
+
+// Obtener el nombre del archivo JSON desde GET
+$jsonFile = isset($_GET['file']) ? $_GET['file'] : 'data.json';
+
+// Cargar y decodificar el contenido del archivo JSON
+$data = json_decode(file_get_contents(__DIR__ . '/data/' . $jsonFile), true);
+
+// Crear una instancia de mPDF
+$mpdf = new Mpdf();
+
+// Crear el contenido HTML del PDF
+$html = "
+<h1>{$data['title']}</h1>
+<p><strong>Author:</strong> {$data['author']}</p>
+<div>{$data['content']}</div>
+";
+
+// Escribir el contenido en el PDF
+$mpdf->WriteHTML($html);
+
+// Generar el PDF
+$mpdf->Output('document.pdf', 'D');
+?>
